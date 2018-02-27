7bc313f6dfb2c491066fe09e884a81d49f60740e
William Sumitro <williamsumitro@gmail.com>
2018-02-27 20:53:44 +0700

    move work

--- app/src/main/java/com/example/williamsumitro/dress/view/splash/splashscreen.java
+++ app/src/main/java/com/example/williamsumitro/dress/view/splash/splashscreen.java
@@ -1,15 +1,20 @@
 package com.example.williamsumitro.dress.view.splash;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.williamsumitro.dress.R;
+import com.example.williamsumitro.dress.view.main.MainActivity;
 
 public class splashscreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splashscreen);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
